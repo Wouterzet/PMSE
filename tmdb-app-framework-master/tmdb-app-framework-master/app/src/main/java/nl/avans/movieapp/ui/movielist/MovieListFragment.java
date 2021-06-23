e456a8ad6858c5d834c47b0609aedfe137e0715a
Wouter Zegers <zegerswouter2@gmail.com>
2021-06-23 19:43:48 +0200

    Update Half working movie lists

---
+++ tmdb-app-framework-master/tmdb-app-framework-master/app/src/main/java/nl/avans/movieapp/ui/movielist/MovieListFragment.java
@@ -0,0 +1,83 @@
+package nl.avans.movieapp.ui.movielist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import java.util.ArrayList;
+
+import nl.avans.movieapp.R;
+import nl.avans.movieapp.domain.Movie;
+import nl.avans.movieapp.ui.movie.MovieDetailActivity;
+
+public class MovieListFragment extends Fragment
+        implements MovieListGridAdapter.OnMovieSelectionListener {
+    private final String LOG_TAG = this.getClass().getSimpleName();
+
+    private MovieListViewModel movieListViewModel;
+    private ArrayList<Movie> mMovies = new ArrayList<>();
+    private RecyclerView mRecyclerView;
+    private MovieListGridAdapter mMoviesGridAdapter;
+
+    private static final int ONE_COLUMN = 1;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        int numGridColumns = ONE_COLUMN;
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(
+                container.getContext(), numGridColumns);
+        mRecyclerView = root.findViewById(R.id.home_movies_grid);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mMoviesGridAdapter = new MovieListGridAdapter(this);
+        mRecyclerView.setAdapter(mMoviesGridAdapter);
+
+        movieListViewModel = new ViewModelProvider(this).get(MovieListViewModel.class);
+        movieListViewModel.getMovies().observe(getViewLifecycleOwner(), new Observer<ArrayList<Movie>>() {
+            @Override
+            public void onChanged(@Nullable ArrayList<Movie> movies) {
+                Log.d(LOG_TAG, "onChanged");
+                mMovies = movies;
+                mMoviesGridAdapter.setMovieList(mMovies);
+            }
+        });
+
+        final SwipeRefreshLayout mSwipeRefreshLayout = root.findViewById(R.id.home_movies_swiperefresh);
+        mSwipeRefreshLayout.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        Log.i(LOG_TAG, "onRefresh called from SwipeRefreshLayout");
+                        // Your action here
+
+                        // Stop the spinner from spinning
+                        mSwipeRefreshLayout.setRefreshing(false);
+                    }
+                }
+        );
+        return root;
+    }
+
+    @Override
+    public void onMovieSelected(int position) {
+        Log.d(LOG_TAG, "onMovieSelected at pos " + position);
+
+        Intent intent = new Intent(getContext(), MovieDetailActivity.class);
+        intent.putExtra("Movie", mMovies.get(position));
+        getContext().startActivity(intent);
+    }
+}
---
+++ tmdb-app-framework-master/tmdb-app-framework-master/app/src/main/java/nl/avans/movieapp/ui/movielist/MovieListGridAdapter.java
@@ -0,0 +1,117 @@
+package nl.avans.movieapp.ui.movielist;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nl.avans.movieapp.R;
+import nl.avans.movieapp.domain.Movie;
+import nl.avans.movieapp.ui.home.HomeGridAdapter;
+
+public class MovieListGridAdapter
+        extends RecyclerView.Adapter<MovieListGridAdapter.MoviesGridViewHolder> {
+
+    private final String LOG_TAG = this.getClass().getSimpleName();
+    private final ArrayList<Movie> moviesArrayList = new ArrayList<>();
+    private final OnMovieSelectionListener listener;
+
+    public MovieListGridAdapter(OnMovieSelectionListener listener) {
+        Log.d(LOG_TAG, "Constructor aangeroepen");
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public MoviesGridViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d(LOG_TAG, "onCreateViewHolder aangeroepen");
+
+        int layoutIdForListItem = R.layout.movie_list_grid_item;
+        final boolean shouldAttachToParentImmediately = false;
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+        return new MoviesGridViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MoviesGridViewHolder holder, int position) {
+        Movie movie = moviesArrayList.get(position);
+        // Log.d(LOG_TAG, "onBindViewHolder");
+
+        Picasso.get()
+                .load(movie.getPoster_path())
+                .resize(700, 700)
+                .centerInside()
+                .into(holder.mMovieImage);
+        if (movie.getTitle() != null) {
+            holder.mMovieTitle.setText(movie.getTitle());
+        }
+        if (movie.getTitle() != null) {
+            holder.mMovieLength.setText(String.valueOf(movie.getRuntime()));
+        }
+        if (movie.getTitle() != null) {
+            holder.mMovieReleaseYear.setText(movie.getRelease_date().substring(0,4));
+        }
+        if (movie.getTitle() != null) {
+            holder.mMovieRating.setText(movie.getVote_average().toString());
+        }
+        Log.d(LOG_TAG, "onBindViewHolder: " + movie);
+    }
+
+    @Override
+    public int getItemCount() {
+        return moviesArrayList.size();
+    }
+
+    public void setMovieList(List<Movie> movies) {
+        Log.d(LOG_TAG, "setMovieList");
+        this.moviesArrayList.clear();
+        this.moviesArrayList.addAll(movies);
+        this.notifyDataSetChanged();
+    }
+
+    public class MoviesGridViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public ImageView mMovieImage;
+        public TextView mMovieTitle;
+        public TextView mMovieLength;
+        public TextView mMovieReleaseYear;
+        public TextView mMovieRating;
+
+
+
+        public MoviesGridViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mMovieImage = (ImageView) itemView.findViewById(R.id.home_movies_griditem_imageurl);
+            mMovieTitle = (TextView) itemView.findViewById(R.id.home_movies_griditem_textview);
+            mMovieLength = (TextView) itemView.findViewById(R.id.home_movies_griditem_lenght);
+            mMovieReleaseYear = (TextView) itemView.findViewById(R.id.home_movies_griditem_releaseYear);
+            mMovieRating = (TextView) itemView.findViewById(R.id.home_movies_griditem_rating);
+
+
+            itemView.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            Log.d(LOG_TAG, "onClick on item " + getAdapterPosition());
+            listener.onMovieSelected(getAdapterPosition());
+
+        }
+    }
+
+    public interface OnMovieSelectionListener {
+        void onMovieSelected(int position);
+    }
+}
