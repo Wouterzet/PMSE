08bd1516d50b202fee78db4bb8ae889604a2ed76
TimdeLaater <tdelaater@gmail.com>
2022-04-06 13:39:45 +0200

    Almost working search

--- tmdb-app-framework-master/tmdb-app-framework-master/app/src/main/java/nl/avans/movieapp/filters/SearchFilter.java
+++ tmdb-app-framework-master/tmdb-app-framework-master/app/src/main/java/nl/avans/movieapp/filters/SearchFilter.java
@@ -1,18 +1,67 @@
 package nl.avans.movieapp.filters;
 
+import android.util.Log;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import me.xdrop.fuzzywuzzy.Applicable;
 import me.xdrop.fuzzywuzzy.FuzzySearch;
 import me.xdrop.fuzzywuzzy.model.BoundExtractedResult;
+import me.xdrop.fuzzywuzzy.model.ExtractedResult;
 import nl.avans.movieapp.domain.Movie;
 
 public class SearchFilter extends FuzzySearch {
-    private ArrayList<Movie> fullList;
+    private List<Movie> filedList;
+    private List<String> nameList;
+    private String search;
     private ArrayList<Movie> filterdList;
 
-    public SearchFilter(List<Movie> filedList, String search) {
-        List<Movie> movies = filedList;
+
+    public SearchFilter(List<Movie> fullList, String search) {
+        this.filedList= fullList;
+        this.search = search;
+        filterdList = new ArrayList<>();
+    }
+    public ArrayList<Movie> filterList(){
+        makeNameList();
+        List<ExtractedResult> filteredNames = new ArrayList<>();
+        filteredNames = FuzzySearch.extractTop(search, nameList,10);
+        Log.d("Search filter",filteredNames.toString());
+        stringsToMovies(filteredNames);
+        return filterdList;
+    }
+//public void filterList(){
+//        makeNameList();
+//        List<ExtractedResult> filteredNames = new ArrayList<>();
+//        filteredNames = FuzzySearch.extractTop(search, nameList,10);
+//        Log.d("Search filter",filteredNames.toString());
+//    }
+    public void stringsToMovies(List<ExtractedResult> filteredNames){
+        for (int i =0;filteredNames.size()> i; i++){
+
+            String name = filteredNames.get(i).toString();
+
+            for (Movie x:filedList
+                 ) {
+                if (x.getTitle() == name){
+                    filterdList.add(x);
+                }
+
+            }
+        }
+    }
+
+
+    public void makeNameList(){
+        nameList = new ArrayList<>();
+        filedList.forEach(name -> nameList.add(name.getTitle()));
 
     }
+
+
+
+
+
 }
